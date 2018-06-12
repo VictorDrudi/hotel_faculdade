b9a5ca80b8c209541601de425dfe7b770ad5ccb6
Victor Drudi <vd28carvalho@gmail.com>
2018-06-12 14:26:09 -0300

    Alterando localização

--- layout/scripts/jquery.maps.js
+++ layout/scripts/jquery.maps.js
@@ -1,5 +1,5 @@
 function initMap() {
-    var uluru = {lat: -23.730857, lng: -46.689765};
+    var uluru = {lat: 19.317704, lng:  -81.383691};
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 18,
       center: uluru
